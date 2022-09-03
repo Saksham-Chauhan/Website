1ff513de071efc4139cc5f0f2e059e5a9755a8b7
saksham <sakshamchauhan79@gmail.com>
2022-09-03 12:59:40 +0530

    responsiveness done

--- src/pages/pricing/Pricing.jsx
+++ src/pages/pricing/Pricing.jsx
@@ -4,19 +4,23 @@ import { ButtonGroup } from "../../components/button-group/ButtonGroup";
 
 import leftBg from "../../assets/Images/bg-left.svg";
 import rightBg from "../../assets/Images/bg-right.svg";
+import leftArr from "../../assets/Images/left-arrow.svg";
+import rightArr from "../../assets/Images/right-arrow.svg";
 
-import "./pricing.css";
 import { Card } from "../../components/card/Card";
 import { basicList } from "../../helper/constant";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { Pagination, Navigation } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/effect-cards";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-// import required modules
-import { EffectCards } from "swiper";
+import "./pricing.css";
 
 export const Pricing = () => {
   return (
@@ -26,37 +30,43 @@ export const Pricing = () => {
       <h1>IT’S EASY TO GET STARTED</h1>
       <p>choose a plan tailored to your needs</p>
       <ButtonGroup />
-      {/* <div className='card-container'>
-        <Card title="Basic" price="$7.9"  list={basicList}/>
-        <Card title="Basic Plus" price="$11.2"  list={basicList}/>
-        <Card title="Premium" price="$12.6"  list={basicList}/>
-        <Card title="Enterprise" price="$13.2"  list={basicList}/>
-      </div> */}
-
-      <Swiper
-        effect={"cards"}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-      >
-        <SwiperSlide style={{ ...styleObj }}>
-          <Card title="Basic" price="$7.9" list={basicList} />
-        </SwiperSlide>
-        <SwiperSlide style={{ ...styleObj }}>
-          <Card title="Basic Plus" price="$11.2" list={basicList} />
-        </SwiperSlide>
-        <SwiperSlide style={{ ...styleObj }}>
-          <Card title="Premium" price="$12.6" list={basicList} />
-        </SwiperSlide>
-        <SwiperSlide style={{ ...styleObj }}>
-          <Card title="Enterprise" price="$13.2" list={basicList} />
-        </SwiperSlide>
-      </Swiper>
+      <div className="card-container">
+        <Card title="Basic" price="$7.9" list={basicList} />
+        <Card title="Basic Plus" price="$11.2" list={basicList} />
+        <Card title="Premium" price="$12.6" list={basicList} />
+        <Card title="Enterprise" price="$13.2" list={basicList} />
+      </div>
+      <div className="mob-view">
+        3
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <Card title="Basic" price="$7.9" list={basicList} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card title="Basic" price="$7.9" list={basicList} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card title="Basic" price="$7.9" list={basicList} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card title="Basic" price="$7.9" list={basicList} />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="swiper-control">
+        <img src={leftArr} alt="" />
+        &nbsp;Swipe&nbsp;
+        <img src={rightArr} alt="" />
+      </div>
     </div>
   );
-};
-
-const styleObj = {
-  transform:
-    "translate3d(calc(-1808px + 7.25%), 0px, -100px) rotateZ(2deg) scale(1) !important",
 };
