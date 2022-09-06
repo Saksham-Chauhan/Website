b82c3a018c515bdc5bfcff1e6c318c34d67ce19b
saksham <sakshamchauhan79@gmail.com>
2022-09-06 17:36:54 +0530

    all changes done

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,9 +3,20 @@ import { Button } from "../button/Button";
 import { ListItem } from "../list-item/ListItem";
 import "./card.css";
 
-export const Card = ({ title, price, list }) => {
+export const Card = ({ title, price, list, translateX, isMobile }) => {
   return (
-    <div className="card">
+    <div
+      className="card mob-card"
+      style={
+        isMobile
+          ? {
+              transform: `translateX(${translateX}px) ${
+                translateX === 0 ? "scale(1.1)" : ""
+              }`,
+            }
+          : {}
+      }
+    >
       <h3>{title}</h3>
       <div>
         <span>{price} </span>/month
